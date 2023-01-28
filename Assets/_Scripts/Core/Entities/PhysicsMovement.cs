95da70fc416acec868c22ec298d04b146e0c494f
Soskar1 <89640069+Soskar1@users.noreply.github.com>
2023-01-28 16:01:44 +0200

    Changes in movement script

--- Assets/_Scripts/Core/Entities/PhysicsMovement.cs
+++ Assets/_Scripts/Core/Entities/PhysicsMovement.cs
@@ -7,10 +7,20 @@ namespace InkeepersKeep.Core.Entities
     {
         [SerializeField] private Rigidbody _rigidbody;
         [SerializeField] private float _speed;
+        [SerializeField] private float _acceleration;
+        [SerializeField] private float _decceleration;
+        [SerializeField] private float _velocityPower;
 
         public void Move(Vector2 direction)
         {
-            _rigidbody.velocity = transform.TransformDirection(new Vector3(direction.x * _speed, _rigidbody.velocity.y, direction.y * _speed));
+            Vector3 transformedDirection = transform.TransformDirection(new Vector3(direction.x, 0f, direction.y));
+            Vector3 targetVelocity = new Vector3(transformedDirection.x * _speed, 0f, transformedDirection.z * _speed);
+            Vector3 velocityDifference = targetVelocity - _rigidbody.velocity;
+            float accelerationRate = (Mathf.Abs(targetVelocity.magnitude) > 0.01f) ? _acceleration : _decceleration;
+            float movementX = Mathf.Pow(Mathf.Abs(velocityDifference.x) * accelerationRate, _velocityPower) * Mathf.Sign(velocityDifference.x);
+            float movementZ = Mathf.Pow(Mathf.Abs(velocityDifference.z) * accelerationRate, _velocityPower) * Mathf.Sign(velocityDifference.z);
+
+            _rigidbody.AddForce(new Vector3(movementX, _rigidbody.velocity.y, movementZ));
         }
     }
 }
