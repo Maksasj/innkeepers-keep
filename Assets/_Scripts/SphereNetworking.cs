d6a07744d669335796081fc22307cfd1ecabc5c6
maksasj <radioboos@gmail.com>
2023-01-31 23:42:49 +0200

    Little improvements

--- Assets/_Scripts/SphereNetworking.cs
+++ Assets/_Scripts/SphereNetworking.cs
@@ -7,8 +7,15 @@ public class SphereNetworking : NetworkBehaviour
 {
     void OnCollisionEnter(Collision collision)
     {
-        if (!IsHost) return;
-        if (!collision.gameObject.TryGetComponent<NetworkBehaviour>(out NetworkBehaviour _object)) return;
-        GetComponent<NetworkObject>().ChangeOwnership(_object.OwnerClientId);
+        if(IsHost)
+        {
+            Debug.Log("Transfering ownership to client");
+            if (!collision.gameObject.TryGetComponent<NetworkObject>(out NetworkObject _object)) return;
+
+            GetComponent<NetworkObject>().ChangeOwnership(_object.OwnerClientId);
+        }
+        //if (!IsHost) return;
+        //if (!collision.gameObject.TryGetComponent<NetworkBehaviour>(out NetworkBehaviour _object)) return;
+        //GetComponent<NetworkObject>().ChangeOwnership(_object.OwnerClientId);
     }
 }
