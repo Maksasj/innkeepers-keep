8dddd41074404710cf3bbc23e7ecb288fbfdce6e
maksasj <radioboos@gmail.com>
2023-02-02 14:44:16 +0200

    Pork leg item prefab variant

--- Assets/_Scripts/Core/Entities/Items/ItemData.cs
+++ Assets/_Scripts/Core/Entities/Items/ItemData.cs
@@ -7,9 +7,4 @@ public class ItemData : ScriptableObject
     [Header("General")]
     public string name;
     public string description;
-
-    [Header("Rendering")]
-    public Mesh mesh;
-    public Material material;
-    public Vector3 scale;
 }
--- Assets/_Scripts/Core/Entities/Items/ItemObject.cs
+++ Assets/_Scripts/Core/Entities/Items/ItemObject.cs
@@ -7,20 +7,9 @@ public class ItemObject : MonoBehaviour
     [Header("General")]
     [SerializeField] private ItemData _itemData;
 
-    [Header("Rendering")]
-    [SerializeField] private MeshFilter _meshFilter;
-    [SerializeField] private Renderer _renderer;
-
-    [SerializeField] private MeshCollider _meshCollider;
-
     private void Awake()
     {
-        _meshFilter.mesh = _itemData.mesh;
-        _renderer.material = _itemData.material;
 
-        transform.localScale = _itemData.scale;
-
-        //_meshCollider.sharedMesh = _itemData.mesh;
     }
 
     void Start()
