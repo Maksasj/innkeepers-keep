9debd164b76d51ca1c2bc0bfda59ba15b0956431
maksasj <radioboos@gmail.com>
2023-02-03 20:33:39 +0200

    Fixed ground check for jumping

--- Assets/_Scripts/Core/Entities/Jumping.cs
+++ Assets/_Scripts/Core/Entities/Jumping.cs
@@ -7,14 +7,19 @@ namespace InkeepersKeep.Core.Entities
         [SerializeField] private Rigidbody _rigidbody;
         [SerializeField] private float _force;
 
-        private bool _onGround;
+        private int _onGround;
 
-        public void OnTriggerEnter(Collider collider) => _onGround = true;
-        public void OnTriggerExit(Collider collider) => _onGround = false;
+        public void OnTriggerEnter(Collider collider) => _onGround += 1;
+        public void OnTriggerExit(Collider collider) => _onGround -= 1;
+
+        private bool OnGround()
+        {
+            return _onGround > 0;
+        }
 
         public void Jump()
         {
-            if (!_onGround) return;
+            if (!OnGround()) return;
 
             _rigidbody.AddForce(Vector2.up * _force, ForceMode.Impulse);
         }
