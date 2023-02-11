2d918fc6fc48b557b0cfafc4bb2b4f74c57d7444
Soskar1 <oskar.vistorskis@gmail.com>
2023-02-11 09:50:30 +0200

    nickname look at camera

--- Assets/_Scripts/Network/PlayerNicknameDisplay.cs
+++ Assets/_Scripts/Network/PlayerNicknameDisplay.cs
@@ -8,7 +8,13 @@ namespace InkeepersKeep.Core.Network
     {
         [SerializeField] private TMP_Text _nicknameText;
 
-        public override void OnNetworkSpawn() => NicknameRequestServerRpc(OwnerClientId);
+        public override void OnNetworkSpawn() 
+        { 
+            NicknameRequestServerRpc(OwnerClientId);
+
+            if (IsOwner)
+                gameObject.SetActive(false);
+        }
 
         [ServerRpc(RequireOwnership = false)]
         private void NicknameRequestServerRpc(ulong clientId)
@@ -20,6 +26,13 @@ namespace InkeepersKeep.Core.Network
         }
 
         [ClientRpc]
-        private void SendNicknameClientRpc(PlayerData playerData) => _nicknameText.text = playerData.PlayerName;
+        private void SendNicknameClientRpc(PlayerData playerData)
+        {
+            if (_nicknameText)
+
+            _nicknameText.text = playerData.PlayerName;
+        } 
+
+        private void LateUpdate() => transform.LookAt(transform.position - Camera.main.transform.forward);
     }
 }
